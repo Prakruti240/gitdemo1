f993b1aa93ee2e5e714446966f484adba49f2c0b
Prakruthi <Prakruti.M@cognizant.com>
2025-07-09 11:25:39 +0530

    commit

--- src/programs/program2.java
+++ src/programs/program2.java
@@ -3,6 +3,8 @@ package programs;
 public class program2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("welcome");
 		System.out.println("bye");
 
+}
 }
